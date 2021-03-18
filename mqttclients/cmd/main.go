0be5d2e993f51c429cd9111fce62f7b2a8ad72a4
KRsully <23422781+KRsully@users.noreply.github.com>
2021-03-18 09:28:37 -0400

    Report Draft Updates

--- mqttclients/cmd/main.go
+++ mqttclients/cmd/main.go
@@ -16,7 +16,7 @@ import (
 func main() {
 	port := flag.String("p", "1883", "port to capture MQTT packets on")
 	brokerAddress := flag.String("b", "127.0.0.1", "IP address or FQDN of the target broker")
-	certFilePath := flag.String("cafile", "certs/ca.crt", "filepath to the certificate of ")
+	certFilePath := flag.String("cafile", "certs/ca.crt", "filepath to the certificate for verifying the server")
 	basicTest := flag.Bool("test-basic", false, "Run basic pub/sub test")
 	basicCertTest := flag.Bool("test-cert", false, "Run basic pub/sub test using a certificate")
 	mqttVersion := flag.Int("-V", 4, "MQTT version 4 for 3.1.1, or 5 for 5.0")
